bb1f20c4e06daf6e3f65a232dacf057436be481e
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-06-05 18:47:55 +0530

    Schema setup

--- src/models/crmModel.js
+++ src/models/crmModel.js
@@ -1,1 +1,27 @@
-er
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = Schema({
+    firstName: {
+        type: String,
+        required: "Enter a first name"
+    },
+    lastName: {
+        type: String,
+        required: "Enter a last name"
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
